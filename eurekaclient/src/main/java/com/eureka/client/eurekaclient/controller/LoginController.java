8bb65bb3d89bfa8c395e3750ae88c043a2b5ce7d
601021997 <42697762+601021997@users.noreply.github.com>
2018-10-17 23:00:55 +0800

    整合mybaits成功

--- eurekaclient/src/main/java/com/eureka/client/eurekaclient/controller/LoginController.java
+++ eurekaclient/src/main/java/com/eureka/client/eurekaclient/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.eureka.client.eurekaclient.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
