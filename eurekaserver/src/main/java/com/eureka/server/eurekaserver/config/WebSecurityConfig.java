ba5f35eb458faa48eae7302ff502ba0a457e1177
601021997 <42697762+601021997@users.noreply.github.com>
2018-10-17 23:23:30 +0800

    整合mybaits成功

---
+++ eurekaserver/src/main/java/com/eureka/server/eurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.eureka.server.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author admin
+ * 新版本解决 Eureka Security 认证问题
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
