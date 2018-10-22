6a0e969e67eb9db4f91a7d5166d8fec45a0ccbaa
wufangyu <wfy601021997>
2018-10-22 09:29:22 +0800

    actuor端点

--- eurekaserver/src/test/java/com/eureka/server/eurekaserver/EurekaServerApplicationTests.java
+++ eurekaserver/src/test/java/com/eureka/server/eurekaserver/EurekaServerApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.function.DoubleSupplier;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaServerApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void demo(){
+        () -> 2
     }
 
 }
