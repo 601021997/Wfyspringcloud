21c19346ba4913a44ec559ab3966f437fc237f28
601021997 <42697762+601021997@users.noreply.github.com>
2018-10-30 00:13:39 +0800

    单例模式

--- foundation/src/test/java/com/java/foundation/IOTest.java
+++ foundation/src/test/java/com/java/foundation/IOTest.java
@@ -5,15 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SocketTest {
+public class IOTest {
+
     @Test
     public void demo1(){
-        //ServerSocket
+
     }
 
 }
